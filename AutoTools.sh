8a7e940755629e9af07a44ee3271645a735a5811
okoklai <60703187+okoklai@users.noreply.github.com>
2022-01-04 13:27:17 +0800

    Add files via upload

---
+++ AutoTools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# AutoTools for XenSystem
+# Powered Aoyouhost
+rm -rf AutoToolsWget.sh
+wget https://my.aoyouhost.com/scripts/AutoToolsWget.sh
+chmod +x AutoToolsWget.sh
+./AutoToolsWget.sh
